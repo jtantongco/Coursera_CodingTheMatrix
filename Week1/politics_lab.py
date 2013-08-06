c5a97e00c33428f79f0dd25c2eb1f6927629c623
jtantongco <jtantongco@gmail.com>
2013-08-06 13:30:10 -0700

    Week1 and Week2 finished up

--- Week1/politics_lab.py
+++ Week1/politics_lab.py
@@ -28,8 +28,18 @@ def create_voting_dict():
 
     The lists for each senator should preserve the order listed in voting data. 
     """
-    return dict() 
+    f = open('voting_record_dump109.txt')
+    data = list(f)
+    senators = []
+    records = []
+    for line in data:
+        bill_history = list(line.split())
+        process_line( bill_history, senators, records)
+    return dict(zip(senators,records)) 
     
+def process_line( votingHistory, senatorsList, recordsList):
+    senatorsList.append(votingHistory[0])
+    recordsList.append([int(votingHistory[x]) for x in range(3, len(votingHistory))])
 
 ## Task 2
 
@@ -44,8 +54,9 @@ def policy_compare(sen_a, sen_b, voting_dict):
         >>> policy_compare('Fox-Epstein','Ravella', voting_dict)
         -2
     """
-    return 0.0
+    return list_dot(voting_dict[sen_a], voting_dict[sen_b])
 
+def list_dot(u,v): return sum([u[i]*v[i] for i in range(len(u))])
 
 ## Task 3
 
@@ -63,9 +74,15 @@ def most_similar(sen, voting_dict):
 
     Note that you can (and are encouraged to) re-use you policy_compare procedure.
     """
-    
-    return ""
-    
+    setSenators = set( voting_dict.keys() ) - {sen}
+    mostAgreeable = 'none'
+    mostAgreeableScore = -1000000    
+    for x in setSenators:
+        agreementScore = policy_compare( x, sen, voting_dict)
+        if agreementScore >= mostAgreeableScore:
+            mostAgreeable = x
+            mostAgreeableScore = agreementScore
+    return mostAgreeable
 
 ## Task 4
 
@@ -80,16 +97,20 @@ def least_similar(sen, voting_dict):
         >>> least_similar('Klein', vd)
         'Ravella'
     """
-    return ""
-    
-    
+    setSenators = set( voting_dict.keys() ) - {sen}
+    leastAgreeable = 'none'
+    leastAgreeableScore = 100000000000
+    for x in setSenators:
+        agreementScore = policy_compare( x, sen, voting_dict)
+        if agreementScore <= leastAgreeableScore:
+            leastAgreeable = x
+            leastAgreeableScore = agreementScore
+    return leastAgreeable
 
 ## Task 5
 
-most_like_chafee    = ''
-least_like_santorum = '' 
-
-
+most_like_chafee    = 'Jeffords' # most_similar('Chafee' , voting_dict)
+least_like_santorum = 'Feingold' # least_similar('Santorum' , voting_dict)
 
 # Task 6
 
@@ -102,10 +123,13 @@ def find_average_similarity(sen, sen_set, voting_dict):
         >>> find_average_similarity('Klein', {'Fox-Epstein','Ravella'}, vd)
         -0.5
     """
-    return ...
+    scores = []
+    for x in sen_set:
+        scores.append(policy_compare( x, sen, voting_dict))
+    return sum(scores)/len(scores)
 
+#apparently ungraded
 most_average_Democrat = ... # give the last name (or code that computes the last name)
-
 
 # Task 7
 
@@ -119,10 +143,40 @@ def find_average_record(sen_set, voting_dict):
         >>> find_average_record({'Fox-Epstein','Ravella'}, voting_dict)
         [-0.5, -0.5, 0.0]
     """
-    return ...
+    sen_votes = [voting_dict[x] for x in sen_set]
+    return averageVotes(sen_votes)
 
-average_Democrat_record = ... # (give the vector)
+def averageVotes(listVotes):
+    averageScore = []
+    numOfVotes = len(listVotes[0])
+    numOfSenators = len(listVotes)
+    for i in range(numOfVotes):
+        averageScore.append(0)
+        for j in range(numOfSenators):
+            averageScore[i] += listVotes[j][i]
+        averageScore[i] = averageScore[i] / numOfSenators
+    return averageScore
 
+#democrats = politics_lab.democrats()
+#vd = politics_lab.create_voting_dict()
+#politics_lab.find_average_record(democrats, vd)
+
+average_Democrat_record = [-0.16279069767441862, -0.23255813953488372, 1.0, 0.8372093023255814, 0.9767441860465116, -0.13953488372093023, -0.9534883720930233, 0.813953488372093, 0.9767441860465116, 0.9767441860465116, 0.9069767441860465, 0.7674418604651163, 0.6744186046511628, 0.9767441860465116, -0.5116279069767442, 0.9302325581395349, 0.9534883720930233, 0.9767441860465116, -0.3953488372093023, 0.9767441860465116, 1.0, 1.0, 1.0, 0.9534883720930233, -0.4883720930232558, 1.0, -0.32558139534883723, -0.06976744186046512, 0.9767441860465116, 0.8604651162790697, 0.9767441860465116, 0.9767441860465116, 1.0, 1.0, 0.9767441860465116, -0.3488372093023256, 0.9767441860465116, -0.4883720930232558, 0.23255813953488372, 0.8837209302325582, 0.4418604651162791, 0.9069767441860465, -0.9069767441860465, 1.0, 0.9069767441860465, -0.3023255813953488] # (give the vector)
+
+#
+
+def democrats():
+    f = open('voting_record_dump109.txt')
+    data = list(f)
+    democrats = set()
+    for line in data:
+        bill_history = list(line.split())
+        if 'D' in bill_history[1]:
+            democrats.add(bill_history[0])
+    return democrats
+
+#politics_lab.find_average_record({'Fox-Epstein','Ravella'}, voting_dict)
+#politics_lab.democrats()
 
 # Task 8
 
@@ -137,5 +191,14 @@ def bitter_rivals(voting_dict):
         >>> bitter_rivals(voting_dict)
         ('Fox-Epstein', 'Ravella')
     """
-    return (..., ...)
-
+    allRivals = { (i,least_similar(i,voting_dict)) for i in voting_dict.keys()}
+    lowScore = 100000000000
+    sen_a = 'none'
+    sen_b = 'none'
+    for (x,y) in allRivals:
+        agreementScore = policy_compare( x, y, voting_dict)
+        if agreementScore < lowScore:
+           sen_a = x
+           sen_b = y
+           lowScore = agreementScore
+    return (sen_a, sen_b)
